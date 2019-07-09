ebf19b1b484a74c28db51ceb9c7d8e12315d45a1
Casey Carroll <44977580+caseydean8@users.noreply.github.com>
2019-07-09 11:53:43 -0400

    Fixed app js syntax (#17)
    * "Got access to deezer api."
    
    * Spotify web playback sdk added.
    
    * Added ticketmaster event api call with zipcode and keyword parameters.
    
    * improved api function and button appearance
    
    * Ticketmaster api now returns result after specified dateTime.
    
    * Page displays name of event on button click.
    
    * Cleaned up code and added comments
    
    * fixed syntax issue with app.js.

--- logicFiles/app.js
+++ logicFiles/app.js
@@ -29,17 +29,29 @@ $(document).on("click", ".mood-button", function(playlist){
         type:"GET",
         url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + genre + "&startDateTime=2019-07-09T00:00:00Z&sort=date,asc&city=" + city + "&apikey=" + apiKey,
 
-    // $(".mood-button").empty();
+    // Empty content holder so it doesn't repeat on button clicks.
+    $("#content-holder").empty();
+
+    // Ticketmaster api key for the project
     var apiKey = "9Ava0NGUIwM3dsiyal9TG4fQF74ykDqb";
+
+    // Keywords variable for api request
     var genre = "rap";
-    // var zipCode = "19123"
+    
+    // City variable for request, user will have to input a city for this to work
     var city = "philadelphia";
-    var startTime = "2019-07-10";
+
+    // moment.js calculations to be added so ticketmaster will only return future events.
+    var currentTime = moment.js;
+
+    // ticketmaster api date format e.g.
+    // dateTime: "2019-07-07T00:00:00Z"
+    
+    // Ajax function taken from ticketmaster documentation https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#search-events-v2
 
     $.ajax({
         type:"GET",
-        url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + genre + "&sort=date,asc&city=" + city + "&apikey=" + apiKey,
-
+        url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + genre + "&startDateTime=2019-07-09T00:00:00Z&sort=date,asc&city=" + city + "&apikey=" + apiKey,
         async:true,
         dataType: "json",
         success: function(json) {
@@ -60,4 +72,10 @@ $(document).on("click", ".mood-button", function(playlist){
                  };
         },
 
+        error: function(xhr, status, err) {
+            // This time, we do not end up here!
+         }
 
+
+    });
+});
