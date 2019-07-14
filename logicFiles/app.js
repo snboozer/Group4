6d67250789fe5bba9696a997467c107286b0e2a2
csaun03 <50086568+csaun03@users.noreply.github.com>
2019-07-14 19:57:05 -0400

    User forum (#34)
    * create user forum
    
    * create user forum
    
    * adding modifications
    
    * updates
    
    * update local b

--- logicFiles/app.js
+++ logicFiles/app.js
@@ -1,17 +1,3 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAQ9upIsSCFqMlxzM2uWqjBMxUTHRtZOMg",
-    authDomain: "fir-58006.firebaseapp.com",
-    databaseURL: "https://fir-58006.firebaseio.com",
-    projectId: "fir-58006",
-    storageBucket: "fir-58006.appspot.com",
-    messagingSenderId: "1033199777878",
-    appId: "1:1033199777878:web:e23d3e8896a47a6a"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 var email;
 var password;
 var city;
